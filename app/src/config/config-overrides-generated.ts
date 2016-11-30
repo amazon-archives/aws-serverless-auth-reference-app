251840aa8f079946b544d127768c61b7ce61e0f0
Justonian <Justonian@users.noreply.github.com>
2016-11-29 23:37:54 -0800

    Adding app config

---
+++ app/src/config/config-overrides-generated.ts
@@ -0,0 +1,8 @@
+// Auto-generated file, do not modify directly
+
+// Empty object indicates that now set config values are overridden.
+const configOverridesGenerated = {
+
+};
+
+export { configOverridesGenerated }
---
+++ app/src/config/config.ts
@@ -0,0 +1,39 @@
+import { configOverridesGenerated } from './config-overrides-generated'
+// DO NOT DELETE THIS FILE!!!
+
+//==============================================
+// Default configurations
+//==============================================
+
+const Config = {
+
+  USER_POOL_ID:               'us-east-1_XWrI9RV17',
+  CLIENT_ID:                  '3dtdumg0i1jrvv23lorfmrlk3l',
+  IDENTITY_POOL_ID:           'us-east-1:992b6d88-74c2-4d5a-9dcd-25e37fa0ff45',
+  REGION:                     'us-east-1',  // Your AWS region where you setup your Cognito User Pool and Federated Identities
+
+  PROFILE_IMAGES_S3_BUCKET:   'spacefinder-api-development-stack-userdatabucket-14zwhc5glptvt',
+
+  API_ENDPOINT:               'https://6jmlncvx8c.execute-api.us-east-1.amazonaws.com/development',
+
+  DEVELOPER_MODE:             false, // enable to automatically login
+  CODE_VERSION:               '1.0.0',
+  DEFAULT_USERNAMES:          ['user1', 'admin1'] // default users cannot change their passwords
+};
+
+//==============================================
+
+
+
+// Merge in the values from the auto-generated config.
+// If there are are conflicts, then the values from the
+// auto-generated config will override
+function mergeConfigurations() {
+  for (let attributeName of Object.keys(configOverridesGenerated)) {
+    Config[attributeName] = configOverridesGenerated[attributeName];
+  }
+}
+
+mergeConfigurations();
+
+export { Config }
