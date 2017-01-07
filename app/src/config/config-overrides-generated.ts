68b3f103518e96efe29e4f3c6b3777fb30734ec1
Vasileios Lekakis (lex) <lekakisv@amazon.com>
2017-01-06 16:20:43 -0800

    keep the config file empty

--- app/src/config/config-overrides-generated.ts
+++ app/src/config/config-overrides-generated.ts
@@ -1,12 +1,7 @@
 // Auto-generated file, do not modify directly
 
 const configOverridesGenerated = {
-  "REGION": "us-east-1",
-  "PROFILE_IMAGES_S3_BUCKET": "spacefinder-api-development-stack-userdatabucket-b97qh513fvny",
-  "API_ENDPOINT": "https://l5sqtnc799.execute-api.us-east-1.amazonaws.com/development",
-  "USER_POOL_ID": "us-east-1_Hgfem46ly",
-  "CLIENT_ID": "bhv3pumspeu8j8f02uhbitbnt",
-  "IDENTITY_POOL_ID": "us-east-1:348c7d1c-82aa-4e42-a5f8-a4176fbe919d"
+
 };
 
 export { configOverridesGenerated }
