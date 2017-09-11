91208e24403319916b51dde2bc9a0839f628f02b
Justin Pirtle <justonian@users.noreply.github.com>
2017-09-11 00:28:53 -0700

    Add build/vendor.js file required for ionic 3.5.2 on index.html

--- app/src/config/config-overrides-generated.ts
+++ app/src/config/config-overrides-generated.ts
@@ -1,6 +1,12 @@
 // Auto-generated file, do not modify directly
 
 const configOverridesGenerated = {
+  "REGION": "us-east-1",
+  "PROFILE_IMAGES_S3_BUCKET": "spacefinder-api-development-stack-userdatabucket-d1ielr6yq989",
+  "API_ENDPOINT": "https://hvuytuhrfg.execute-api.us-east-1.amazonaws.com/development",
+  "USER_POOL_ID": "us-east-1_LbkuBIw4G",
+  "CLIENT_ID": "4ou279pbj1tn43hms8bfsguf1i",
+  "IDENTITY_POOL_ID": "us-east-1:89487d88-fbbc-4ec5-9c02-60661ab92629"
 };
 
 export { configOverridesGenerated }
