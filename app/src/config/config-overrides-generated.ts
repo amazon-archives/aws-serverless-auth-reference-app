038e103e8c116637c16c8c0bb00efb6fb4c29d5c
Justin Pirtle <justonian@users.noreply.github.com>
2017-10-30 19:17:16 -0500

    Resetting config overrides to default state

--- app/src/config/config-overrides-generated.ts
+++ app/src/config/config-overrides-generated.ts
@@ -1,14 +1,6 @@
 // Auto-generated file, do not modify directly
 
 const configOverridesGenerated = {
-  "REGION": "us-east-1",
-  "PROFILE_IMAGES_S3_BUCKET": "spacefinder-dev-stack-userdatabucket-i6nj14r1a174",
-  "API_ENDPOINT": "https://423ufnimra.execute-api.us-east-1.amazonaws.com/dev",
-  "USER_POOL_ID": "us-east-1_Rtvu3D6En",
-  "USER_POOL_DOMAIN_NAME": "spacefinder-dev-us-east-1-rtvu3d6en.auth.us-east-1.amazoncognito.com",
-  "USER_POOL_DOMAIN_PREFIX": "spacefinder-dev-us-east-1-rtvu3d6en",
-  "CLIENT_ID": "1d1rikcld5s6ck51l3kea9o51f",
-  "IDENTITY_POOL_ID": "us-east-1:567db671-5aae-4042-aa21-2f8cb599ee41"
 };
 
 export { configOverridesGenerated }
--- app/src/config/config.ts
+++ app/src/config/config.ts
@@ -7,16 +7,16 @@ import { configOverridesGenerated } from './config-overrides-generated'
 
 const Config = {
 
-  USER_POOL_ID:               'us-east-1_XWrI9RV17',
-  USER_POOL_DOMAIN_NAME:      'spacefinder-dev-us-east-1-opikzdefz.auth.us-east-1.amazoncognito.com',
-  USER_POOL_DOMAIN_PREFIX:    'spacefinder-dev-us-east-1-opikzdefz',
-  CLIENT_ID:                  '3dtdumg0i1jrvv23lorfmrlk3l',
-  IDENTITY_POOL_ID:           'us-east-1:992b6d88-74c2-4d5a-9dcd-25e37fa0ff45',
+  USER_POOL_ID:               'us-east-1_79D6i69td',
+  USER_POOL_DOMAIN_NAME:      'spacefinder-development-us-east-1-79d6i69td.auth.us-east-1.amazoncognito.com',
+  USER_POOL_DOMAIN_PREFIX:    'spacefinder-development-us-east-1-79d6i69td',
+  CLIENT_ID:                  '58pvvcoktlrhcng34ve68i74uf',
+  IDENTITY_POOL_ID:           'us-east-1:aac5e9e6-ff3a-4586-b984-2cd88b693f69',
   REGION:                     'us-east-1',  // Your AWS region where you setup your Cognito User Pool and Federated Identities
 
-  PROFILE_IMAGES_S3_BUCKET:   'spacefinder-api-development-stack-userdatabucket-14zwhc5glptvt',
+  PROFILE_IMAGES_S3_BUCKET:   'spacefinder-development-stack-userdatabucket-11wtuhxdpzbp5',
 
-  API_ENDPOINT:               'https://6jmlncvx8c.execute-api.us-east-1.amazonaws.com/development',
+  API_ENDPOINT:               'https://g2drvofsu8.execute-api.us-east-1.amazonaws.com/development',
 
   DEVELOPER_MODE:             false, // enable to automatically login
   CODE_VERSION:               '1.0.0',
